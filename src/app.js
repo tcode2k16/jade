420803d02384bded8ada14ba44bf6ace51e1c9b6
tcode2k16 <alanc@macbook.local>
2017-01-08 16:25:19 +0800

    WIP

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Item from './class/Item';
 
 const WIDTH = 8;
-const HEIGHT = 10;
+const HEIGHT = 11;
+const DELAY = 100;
 
 class Game {
     constructor(op) {
+        this.moving = false;
         this.screenWidth = window.innerWidth;
         this.blocks = [];
         this.root = op.root;
@@ -19,27 +21,41 @@ class Game {
             'whiteGem': 7
         };
 
-        for (let y = 0; y < HEIGHT; y++) {
-            this.blocks.push([]);
-            for (let x = 0; x < WIDTH; x++) {
-                this.blocks[y].push(new Item({
-                    type: this.randomItemType(),
-                    root: this.gameBoard,
-                    x,
-                    y
-                }));
-            }
-        }
+        
+        this.init_blocks()
 
         console.log(this.toString());
+        console.log(this.haveMatch(this.toString()));
+        
+        this.removeMatches(true)
 
         window.addEventListener('resize', this.onResize.bind(this))
 
-        console.log(this.haveMatch(this.toString()));
         
         window.requestAnimationFrame(function() {
             this.root.style.transform = 'scale('+(this.screenWidth/150)+')';
         }.bind(this))
+    }
+
+    init_blocks() {
+        this.blocks = []
+        for (let x = 0; x < WIDTH; x++) {
+            this.blocks.push([]);
+            for (let y = 0; y < HEIGHT; y++) {
+                let it = new Item({
+                    type: this.randomItemType(),
+                    root: this.gameBoard,
+                    parent: this,
+                    x,
+                    y
+                });
+                this.blocks[x].push(it);
+            }
+        }
+    }
+
+    handleClick(el) {
+        el.classList.toggle('selected')
     }
 
     randomItemType() {
@@ -53,18 +69,137 @@ class Game {
     }
 
 
-    haveMatch(s) {
-
+    haveMove(s) {
         return /(\d)(\1(\d|.{6}|.{9})|(\d|.{6}|.{9})\1|.{7}\1(.|.{9})|(.|.{9})\1.{7}|(.{7,9}|.{17})\1.{8}|.{8}\1(.{7,9}|.{17}))\1/.test(s.replace(/\n/g,'A'))
+    }
 
+    haveMatch(s) {
+        return /(\d)(\1{2,}|(.{8}\1){2,})/.test(s.replace(/\n/g,'A'))
+    }
+
+    removeMatches(init = false) {
+        this.moving = true;
+        if (init) {
+            let rm = []
+            for (let y = 1; y < HEIGHT; y++) {
+                for (let x = 0; x < WIDTH; x++) {
+                    if (this.isVMatch(x,y)||this.isHMatch(x,y))
+                        rm.push(this.blocks[x][y])
+                }
+            }
+
+            rm.map(function (e) {
+                this.removeItem(e)
+            }.bind(this))
+            this.update();
+
+            console.log(this.haveMatch(this.toString()));
+
+            if (!this.haveMatch(this.toString())) {
+                this.moving=false;
+                return 1;
+            }
+
+            this.removeMatches(init)
+        }else {
+            setTimeout(function() {
+                let rm = []
+                for (let y = 1; y < HEIGHT; y++) {
+                    for (let x = 0; x < WIDTH; x++) {
+                        if (this.isVMatch(x,y)||this.isHMatch(x,y))
+                            rm.push(this.blocks[x][y])
+                    }
+                }
+
+                rm.map(function (e) {
+                    this.removeItem(e)
+                }.bind(this))
+                this.update();
+
+                console.log(this.haveMatch(this.toString()));
+
+                if (!this.haveMatch(this.toString())) {
+                    this.moving=false;
+                    return 1;
+                }
+
+                this.removeMatches(init)
+            }.bind(this),DELAY);
+        }
+        
+            
+            
+        
+    }
+
+    removeItem(item,rm) {
+        item.setType(this.randomItemType())
+        this.blocks[item.x].splice(item.y,1);
+        this.blocks[item.x].unshift(item);
+
+    }
+
+    update() {
+        for (let x=0; x<this.blocks.length; x++)
+            for (let y=0; y< this.blocks[x].length; y++) {
+                let item = this.blocks[x][y]
+                if (item.x !== x || item.y !== y)
+                    item.setXY(x,y)
+            }
+                    
+    }
+
+    isVMatch(x,y) {
+        let matchUp = 0;
+        let matchDown = 0;
+        let item = this.blocks[x][y];
+        let type = item.type;
+
+        //up
+        for (let i = y-1; i>0; i--)
+            if (type===this.blocks[x][i].type)
+                matchUp++;
+            else
+                break;
+        //down
+        for (let i=y+1; i<this.blocks[x].length; i++)
+            if (type===this.blocks[x][i].type)
+                matchDown++;
+            else
+                break;
+        
+        return matchUp+matchDown >= 2 ? true : false;
+    }
+
+    isHMatch(x,y) {
+        let matchLeft = 0;
+        let matchRight = 0;
+        let item = this.blocks[x][y];
+        let type = item.type;
+
+        //left
+        for (let i = x-1; i>=0; i--)
+            if (type===this.blocks[i][y].type)
+                matchLeft++;
+            else
+                break;
+        //right
+        for (let i= x+1; i<this.blocks.length; i++)
+            if (type===this.blocks[i][y].type)
+                matchRight++;
+            else
+                break;
+        
+        return matchLeft+matchRight >= 2 ? true : false;
     }
 
     toString() {
         let str = '';
-        for (let row of this.blocks) {
-            for (let item of row)
-                str += this.itemTypes[item.type];
-            str+="\n";
+        for (let y = 1; y < HEIGHT; y++) {
+             for (let x = 0; x < WIDTH; x++) {
+                str+=this.itemTypes[this.blocks[x][y].type];
+            }
+            str+='\n';
         }
         return str;
 
--- src/class/Item.js
+++ src/class/Item.js
@@ -9,44 +9,68 @@ export default class Item {
         //     y: y
         // }
 
-        this.type = op.type;
-        this.x = op.x;
-        this.y = op.y;
-
-
-        let item = document.createElement('div');
-        item.classList.add(op.type, 'item');
+        this.parent = op.parent;
         
+
+        this.type = op.type;
+        this.item = document.createElement('div');
+        this.item.classList.add('item',op.type);
+        this.setMargin()
 
         this.el = document.createElement('div');
-        this.el.appendChild(item);
+        this.el.appendChild(this.item);
         this.el.classList.add('tile');
+        this.setXY(op.x,op.y);
         
-        
-        // this.el.style.transform = "translate3d()"
         op.root.appendChild(this.el);
 
+
+        this.setUpHammer();
+                
+
+    }
+
+
+    setType(t) {
+        if (t === this.type)
+            return;
         
+        this.item.classList.remove(this.type);
+        this.item.classList.add(t);
+        this.type=t;
+        this.setMargin();
+    }
+
+    setXY(x,y) {
+        
+
+        this.x=x;
+        this.y=y;
+
         window.requestAnimationFrame(function() {
-            this.el.style.transform = 'translate3d('+(op.x*16)+'px,'+(op.y*16)+'px,0px)';
-            item.style.marginLeft = (-item.clientWidth/2) + "px";
-            item.style.marginTop = (-item.clientHeight/2) + "px";
+            this.el.style.transform = 'translate3d('+(x*16)+'px,'+((y-1)*16)+'px,0px)';
+        }.bind(this))
+
+        // if (y===0)
+        //     this.el.classList.add('hidden')
+        // else if (this.el.classList.contains('hidden'))
+        //     this.el.classList.remove('hidden')
+    }
+
+    setMargin() {
+        window.requestAnimationFrame(function() {
+            this.item.style.marginLeft = (-this.item.clientWidth/2) + "px";
+            this.item.style.marginTop = (-this.item.clientHeight/2) + "px";
             
         }.bind(this));
+    }
 
-
+    setUpHammer() {
         this.hammer = new Hammer.Manager(this.el);
         this.hammer.add(new Hammer.Tap())
 
-        this.hammer.on('tap', this.onTap.bind(this))
+        this.hammer.on('tap', this.parent.handleClick.bind(this.parent));
 
-        // console.log(op);
-    }
-
-    onTap(ev) {
-        // console.log(ev);
-        this.el.classList.toggle('selected')
-        
     }
 
 
